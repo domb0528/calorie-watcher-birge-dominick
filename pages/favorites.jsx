4c43a250fe7d32b1aca83f44d7e3bf9dedb37b91
Dominick birge <dominickbirge@Dominicks-MacBook-Pro.local>
2023-08-10 16:29:26 -0600

    fixing deployment

--- pages/favorites.jsx
+++ pages/favorites.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/home.module.css'
 import Header from "../components/header";
 
 
